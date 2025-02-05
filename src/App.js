f3c2999b1079adea25b407d45d08281e89696243
Ankur Pata <ankur.patankar@timedoctor.com>
2025-02-05 22:29:19 +0400

    feat: add routing for category and listing pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://www.facebook.com"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
