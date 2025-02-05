f0a7208e7b313320c60705a6d065f5ac867d3b98
Ankur Pata <ankur.patankar@timedoctor.com>
2025-02-05 22:06:27 +0400

    feat: add routing for category and listing pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ListingPage from './ListingPage';
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <Router>
       <div className="App">
         <Switch>
@@ -18,6 +19,60 @@ function App() {
         </Switch>
       </div>
     </Router>
+=======
+    <div className="App">
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+=======
+      <h1>Hello World</h1>
+      <ProductListing />
+>>>>>>> Stashed changes
+=======
+      <h1>Hello World</h1>
+      <ProductListing />
+>>>>>>> Stashed changes
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
