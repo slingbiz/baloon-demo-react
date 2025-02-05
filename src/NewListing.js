0aecfa175914a06afea6699d8376b4e386c50d75
Ankur Pata <ankur.patankar@timedoctor.com>
2025-02-05 21:57:08 +0400

    feat: add basic routing and page components for app setup

---
+++ src/NewListing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewListing = () => {
+  return (
+    <div>
+      <h1>Create New Product Listing</h1>
+      {/* Add form for creating a new product listing */}
+    </div>
+  );
+};
+
+export default NewListing;
---
+++ src/components/ProductListing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/ProductListing.css';
+
+function ProductListing() {
+  return (
+    <div className="product-listing">
+      <h2>Featured Products</h2>
+      {/* Add product listing components here */}
+    </div>
+  );
+}
+
+export default ProductListing;
